70849d9b35fce889f94469f567c7222cf2336989
GospelExcel <gexcel123@gmail.com>
2025-03-11 18:46:34 -0400

    Update database connection to use PostgreSQL if DATABASE_URL is provided

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./url_shortener.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./url_shortener.db")
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}  
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
